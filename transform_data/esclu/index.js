92af6ce874cd30a532cca4da0dfed7e855542937
mannixchan <51387089+mannixchan@users.noreply.github.com>
2023-03-13 08:30:28 +0800

    Update index.js

--- transform_data/esclu/index.js
+++ transform_data/esclu/index.js
@@ -4,55 +4,84 @@ const program = require('commander')
 const pkg = require('./package.json');
 
 const fullUrl = (path = '') => {
-	let url = `http://${program.host}:${program.port}/`
-	if(program.index) {
-		url += program.index + '/'
-		if(program.type) {
-			url += program.type + '/'
-		}
-	}
-	return url + path.replace(/^\/*/, '')
+  let url = `http://${program.host}:${program.port}/`
+  if (program.index) {
+    url += program.index + '/'
+    if (program.type) {
+      url += program.type + '/'
+    }
+  }
+  return url + path.replace(/^\/*/, '')
 }
 
 program
-	.version(pkg.version)
-	.description(pkg.description)
-	.usage('[options] <command> [...]')
-	.option('-o, --host <hostname>', 'hostname [localhost]', 'localhost')
-	.option('-p, --port <number>', 'port number [9200]', '9200')
-	.option('-j, --json', 'format output as JSON')
-	.option('-i, --index <name>', 'which index to use')
-	.option('-t, --type <type>', 'default type for bulk operation');
+  .version(pkg.version)
+  .description(pkg.description)
+  .usage('[options] <command> [...]')
+  .option('-o, --host <hostname>', 'hostname [localhost]', 'localhost')
+  .option('-p, --port <number>', 'port number [9200]', '9200')
+  .option('-j, --json', 'format output as JSON')
+  .option('-i, --index <name>', 'which index to use')
+  .option('-t, --type <type>', 'default type for bulk operation');
 program.command('url [path]')
-	.description('generate the URL for the options and path (default is /)')
-	.action((path = '/') =>
-	{
-		console.log(fullUrl(path))
-	})
+  .description('generate the URL for the options and path (default is /)')
+  .action((path = '/') => {
+    console.log(fullUrl(path))
+  })
 
 program.command('get [path]')
-	.description('Perform HTTP GET (default is /)')
-	.action((path = '/') =>
-	{
-		const options = {
-			url: fullUrl(path),
-			json: program.json
-		}
-		request(options, (err, res, body) => {
-			if(program.json) {
-				console.log(JSON.stringify(err || body))
-			}
-			else {
-				if(err) throw err;
-				console.log(body);
-			}
-		})
-	})
+  .description('Perform HTTP GET (default is /)')
+  .action((path = '/') => {
+    const options = {
+      url: fullUrl(path),
+      json: program.json
+    }
+    request(options, (err, res, body) => {
+      if (program.json) {
+        console.log(JSON.stringify(err || body))
+      } else {
+        if (err) throw err;
+        console.log(body);
+      }
+    })
+  })
 
+program.command('create-index')
+  .description('create an index')
+  .action(() => {
+    if (!program.index) {
+      const msg = "No index specified! USE --index <name>"
+      if (!program.json) throw Error(msg)
+      console.log(JSON.stringify({error: msg}));
+      return
+    }
+    request.put(fullUrl(), handleResponse)
+  })
+
+program
+	.command('list-indices')
+	.alias('li')
+	.description('get a list of indices in this cluster')
+	.action(() => {
+		const path = program.json ? '_all' : '_cat/indices?v'
+    request({url: fullUrl(path), json: program.json}, handleResponse)
+	})
 
 
 program.parse(process.argv)
 
-if(!program.args.filter(arg => typeof arg === 'object').length) {
-	program.help();
+if (!program.args.filter(arg => typeof arg === 'object').length) {
+  program.help();
+}
+
+
+function handleResponse(err, res, body) {
+  if (program.json) {
+    console.log(JSON.stringify(err || body));
+  } else {
+    if (err) {
+      throw err
+    }
+    console.log(body);
+  }
 }
